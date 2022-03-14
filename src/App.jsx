672b4c1456dd83a38a1483d8e1a4409a78f9302b
GoAlive13 <95854153+GoAlive13@users.noreply.github.com>
2022-03-14 10:44:57 +0530

    third commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import "./App.css";
 import { Rentals } from "./Components/Rentals/Rentals";
 import { AddHouse } from "./Components/AddHouse/AddHouse";
+import { useState } from "react";
 
 function App() {
+  const[visible,setVisible]=useState(true)
   return (
     <div className="App">
-      <button className="toggleForm">
+
+    {visible?<Rentals/>:<AddHouse/>}
+      <button className="toggleForm" onClick={() => {
+        setVisible(visible ? false : true);
+      }}>
+       
+       
         {/* Show text Add House or Show Rentals based on state */}
+       {visible?"AddHouse":"Show Rentals"}
+        
       </button>
       {/* Show component based on state */}
       <br />
