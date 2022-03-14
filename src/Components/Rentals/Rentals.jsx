6700457faa3ae22a592933e3ffce362568d257ec
GoAlive13 <95854153+GoAlive13@users.noreply.github.com>
2022-03-14 11:59:11 +0530

    commit

--- src/Components/Rentals/Rentals.jsx
+++ src/Components/Rentals/Rentals.jsx
@@ -1,18 +1,46 @@
 // import { useState } from "react";
 import "./Rentals.css";
 
-import data from "../../../db.json";
+import data1 from "../../../db.json";
+import { useState } from "react";
 
 export const Rentals = () => {
+  const list = data1.houses;
+  const [data, showData] = useState(list);
+
+
+  const Id=()=>{
+    data.sort(function (a,b) {return b.id-a.id});
+    showData(data);
+  }
+  const rentascending=()=>{
+    data.sort(function (a,b) {return a.rent-b.rent});
+     showData([...data]);
+  }
+  const rentdescending=()=>{
+    data.sort(function (a,b) {return b.rent-a.rent});
+     showData([...data]);
+  }
+  const aresascending=()=>{
+    data.sort(function (a,b) {return a.areaCode-b.areaCode});
+    showData([...data]);
+  }
+  const areadescending=()=>{
+    data.sort(function (a,b) {return b.areaCode-a.areaCode});
+    showData([...data]);
+  }
+
+
+
   // const [house, setHouse] = useState(data.houses);
   return (
     <div className="rentalContainer">
       <div className="sortingButtons">
-        <button className="sortById">Sort by ID</button>
-        <button className="sortByRentAsc">Rent Low to high</button>
-        <button className="sortByRentDesc">Rent High to low</button>
-        <button className="sortByAreaAsc">Area Low to high</button>
-        <button className="sortByAreaDesc">Area High to Low</button>
+        <button className="sortById" onClick={Id}>Sort by ID</button>
+        <button className="sortByRentAsc"onClick={rentascending}>Rent Low to high</button>
+        <button className="sortByRentDesc" onClick={rentdescending}>Rent High to low</button>
+        <button className="sortByAreaAsc"  onClick={aresascending}>Area Low to high</button>
+        <button className="sortByAreaDesc" onClick={areadescending}>Area High to Low</button>
       </div>
       <input
         className="searchAddress"
@@ -33,21 +61,21 @@ export const Rentals = () => {
           </tr>
         </thead>
         <tbody>
-          {data.houses.map((house, index) => {
+          {data1.houses.map((houses, index) => {
             return (
-              <tr key={house.id} className="houseDetails">
-                <td className="houseId">{house.id}</td>
-                <td className="houseName">{house.name} </td>
-                <td className="ownersName">{house.ownerName}</td>
-                <td className="address">{house.address}</td>
-                <td className="areaCode">{house.areaCode}</td>
-                <td className="rent">{house.rent}</td>
+              <tr key={houses.id} className="houseDetails">
+                <td className="houseId">{houses.id}</td>
+                <td className="houseName">{houses.name} </td>
+                <td className="ownersName">{houses.ownerName}</td>
+                <td className="address">{houses.address}</td>
+                <td className="areaCode">{houses.areaCode}</td>
+                <td className="rent">{houses.rent}</td>
                 <td className="preferredTenants">
-                  {/* Show text Both or Bachelors or Married based on values */ house.preferred_tenants}
+                  {/* Show text Both or Bachelors or Married based on values */ houses.preferred_tenants}
 
                 </td>
                 <td className="houseImage">
-                  <img src={house.image} alt="house" />
+                  <img src={houses.image} alt="house" />
                 </td>
               </tr>
             );
